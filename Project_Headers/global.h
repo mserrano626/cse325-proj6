5222baa1ccc2f935e82872a66060587a7047d7ba
maserra3 <maserra3@asu.edu>
2013-04-12 21:07:56 +0000

    still trying to get the LEDM to work

---
+++ Project_Headers/global.h
@@ -0,0 +1,17 @@
+/*
+ * global.h
+ *
+ *  Created on: Apr 12, 2013
+ *      Author: maserra3
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include "itrc.h"
+#include "support_common.h"
+
+extern void led_refresh();
+extern void qspi_transmit(uint8 data[]);
+
+#endif /* GLOBAL_H_ */
